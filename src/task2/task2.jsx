215e6ff3279ce7165f14036cbbbef073466fae98
Rahul Kumawat <kumawat30rahul@gmail.com>
2025-06-23 15:26:08 +0530

    fix: build fixes for task 2

--- src/task2/task2.jsx
+++ src/task2/task2.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import BackButton from "../components/backButton/BackButton";
 import CommentsTable from "../components/commentsTable/CommentsTable";
-import CommonButton from "../components/commonButton/commonButton";
-import ProductsTable from "../components/products/productsTable.jsx";
+import ProductsTable from "../components/products/productsTable";
+import CommonButton from "../components/commonButton/CommonButton";
 
 const TaskTwo = () => {
   const [selectedTab, setSelectedTab] = useState("products");
